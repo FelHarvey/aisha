7132d75a3cc1434f5b68fbe9aef4a3e2e2c16004
felharvey <felicity@quirkycreativesupplies.com.au>
2026-01-23 22:54:48 +1100

    1.4 passed

--- main.py
+++ main.py
@@ -13,4 +13,13 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
+
+if response.usage_metadata is None:
+    raise RuntimeError("No usage metadata!")
+
+prompt_tokens = response.usage_metadata.prompt_token_count
+response_tokens = response.usage_metadata.candidates_token_count
+
+print(f"Prompt tokens: {prompt_tokens}")
+print(f"Response tokens: {response_tokens}")
 print(response.text)
