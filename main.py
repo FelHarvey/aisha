1ccc760ce56c363342b70cf786daf26469e16df6
felharvey <felicity@quirkycreativesupplies.com.au>
2026-01-24 21:13:44 +1100

    Finished chapter 1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import all needed outside info
-import os
+import os, argparse
+from google.genai import types
 from dotenv import load_dotenv
 from google import genai
 
@@ -10,16 +11,30 @@ if api_key is None:
 
 client = genai.Client(api_key=api_key)
 
+# Code block to allow user to imput prompts
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+# Store a list of user prompts
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+# The actual generation of content
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model='gemini-2.5-flash', contents=messages
 )
 
+# Code block regarding token usage
 if response.usage_metadata is None:
     raise RuntimeError("No usage metadata!")
 
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
 
-print(f"Prompt tokens: {prompt_tokens}")
-print(f"Response tokens: {response_tokens}")
+# Printing the results
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
 print(response.text)
